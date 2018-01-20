11866edea660d62068130cf59f704053d2d07f04
William <willshinji@gmail.com>
2018-01-20 21:43:32 -0200

    fixing key from map example

--- examples/maps_weakmaps/map_iterator.js
+++ examples/maps_weakmaps/map_iterator.js
@@ -3,7 +3,7 @@ var cities = new Map();
 cities.set(1, 'São Paulo');
 cities.set(2, 'São José dos Campos');
 cities.set(3, 'Parati');
-cities.set(1, 'Praia Grande');
+cities.set(4, 'Praia Grande');
 
 
 cities.get(2);
