c972cc64879cbfbad48f5acd53a4833eccdcb69b
William <willshinji@gmail.com>
2018-01-27 19:32:47 -0200

    practicing auxiliar array methods

--- exercises/array methods/answers.js
+++ exercises/array methods/answers.js
@@ -107,3 +107,28 @@ function existeProdutosDatados(produtos, data){
 
 existeProdutosDatados(produtos, '2017-03-01');
 existeProdutosDatados(produtos,'2016-01-01');
+
+
+
+//Você está muito longe
+//Implemente uma função chamada calculaDistancia que recebe uma lista de objetos que representam ruas e faça a soma de seus tamanhos. Cada objeto rua da lista possui as seguintes propriedades:
+
+//nome: String que representa o nome da rua
+//tamanho: Número intero que representa a comprimento da rua em metros
+//Utilize somente os conceitos que foram apresentados neste capítulo para iterar as ruas. Assuma que sempre haverá pelo menos uma rua no array.
+
+//Tome como exemplo, a entrada abaixo.
+
+var ruas = [
+  { nome:'Rua 1', tamanho: 2500 },
+  { nome:'Rua 2', tamanho: 3400 },
+  { nome:'Rua 3', tamanho: 1400 }
+];
+
+function calculaDistancia(ruas){
+  return ruas.reduce(function(distancia, rua){
+      return distancia + rua.tamanho;
+  }, 0);
+}
+
+calculaDistancia(ruas);
