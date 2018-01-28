2e99123a016a09c7ec0db7e684a3d7d47669c5ab
William <willshinji@gmail.com>
2018-01-28 13:48:37 -0200

    let example

---
+++ examples/var, let and const/var_let_const.js
@@ -0,0 +1,20 @@
+//var usage
+//function block(){
+//  var message = 'some message';
+//  if(true){
+//    var message = 'another message';
+//  }
+//  console.log(message);
+//}
+
+
+//let usage
+function block(){
+  let message = 'some message';
+  if(true){
+    let message = 'another message';
+  }
+  console.log(message);
+}
+
+block();
