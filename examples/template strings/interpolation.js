5d4183c51aa1fe8f0b87e60adf26db9bd5269539
William <willshinji@gmail.com>
2018-02-06 13:36:09 -0200

    template strings example

---
+++ examples/template strings/interpolation.js
@@ -0,0 +1,19 @@
+//breaking lines - traditional
+console.log("a\word")
+
+//breaking lines - ES6
+console.log(`
+a 
+word`);
+
+
+//easy interpolation
+let name = "william"
+console.log("Greetings "+ name)
+console.log(`Greetings ${name}`)
+
+const n1 = 1;
+const n2 = 3;
+
+console.log(n1+" + "+n2+" = "+(n1+n2))
+console.log(`${n1} + ${n2} = ${n1+n2}`)
