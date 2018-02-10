5266ea963a5e6ac6f41bab357d95b62a8bec83a1
William <willshinji@gmail.com>
2018-02-10 12:36:58 -0200

    arrow functions

---
+++ examples/arrow functions/arrow.js
@@ -0,0 +1,8 @@
+//ES5
+//var greetings = function(name){
+//  console.log(`Greetings ${name}`)}
+
+//ES6 arrow function
+const greetings = (name) => `Greeting ${name}`
+
+console.log(greetings('william'))
