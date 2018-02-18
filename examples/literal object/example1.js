9944b17bb2577f0d686aaf7058f44d2178e27fb4
William <willshinji@gmail.com>
2018-02-18 13:39:18 -0300

    object literal cleaner code

---
+++ examples/literal object/example1.js
@@ -0,0 +1,21 @@
+//ES5
+//var name = 'Beatriz';
+//var age = 3;
+
+//var rabbit = {
+//  name : name,
+//  age : age
+//}
+
+//console.log(rabbit.name+" has "+rabbit.age+ " years old")
+
+
+//ES6 - Clean code
+let name = 'Chapo';
+let age = 2;
+
+const rabbit = {
+  name, age
+}
+
+console.log(`${rabbit.name} has ${rabbit.age} years old`)
