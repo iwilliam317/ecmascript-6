d0fc6a9a9cd4385fb10e4016787ca162aa053df9
William <willshinji@gmail.com>
2018-02-28 11:15:22 -0300

    rest example

---
+++ examples/rest/rest1.js
@@ -0,0 +1,12 @@
+//rest for n parameters
+function sum(...values){
+  let outcome = 0;
+  
+  for(var i=0; i<values.length; i++){
+    outcome += outcome + values[i];
+  }
+  console.log(`Outcome: ${outcome}`)
+}
+
+sum(1)
+sum(1,2)
