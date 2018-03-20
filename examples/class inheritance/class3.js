7b7e9883e92eecae1a096e1917c344c459882b61
william <willshinji@gmail.com>
2018-03-20 13:48:46 -0300

    class inheritance es6 example

---
+++ examples/class inheritance/class3.js
@@ -0,0 +1,25 @@
+class Vehicle {
+  constructor(model, year){
+    this.model = model;
+    this.year = year;
+  }
+  
+  info(){
+    console.log(`${this.model} ${this.year}`)
+  }
+}
+
+class Car extends Vehicle {
+  constructor(model, year, color) {
+    super(model, year)
+    this.color = color;
+  }
+  
+  info(){
+    console.log(`${this.model} ${this.year} ${this.color}`)
+  }
+  
+}
+
+let car = new Car('Lamborghini Huracan', 2016, 'blue');
+car.info();
