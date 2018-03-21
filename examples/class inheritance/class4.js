da2997df695c27d6c462a1ece37a710785d0b5c8
william <willshinji@gmail.com>
2018-03-21 14:29:04 -0300

    static methods for class

---
+++ examples/class inheritance/class4.js
@@ -0,0 +1,15 @@
+//static methods
+class Tv {
+  
+  static turnOn(){
+    console.log("turning on TV..");
+  }
+  
+  static turnOff(){
+    console.log("turning off TV..");
+  }
+
+}
+
+Tv.turnOn();
+Tv.turnOff();
