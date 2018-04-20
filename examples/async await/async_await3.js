7e4f564263c51b9d5256bd605d857ee83e4829e0
william <willshinji@gmail.com>
2018-04-20 00:22:19 -0300

    what can i say, practing is perfection's mother lol

---
+++ examples/async await/async_await3.js
@@ -0,0 +1,21 @@
+function sleep(amount){
+  return new Promise((resolve, reject) => {
+    if(amount <= 300){
+      reject("that is too fast, cool it down");
+    }
+    setTimeout(() => resolve(`slept for ${amount}`) ,amount)
+  })
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
+}
+
+
+
+async function start(){
+  await sleep(200);
+  console.log('taking a break..')
+  await sleep(750);
+  await sleep(1000);
+}
+
+start();
