0790c3fb2ad8295d76d3b82575c7c6ae97201a6b
william <willshinji@gmail.com>
2018-08-08 13:31:17 -0300

    adds more examples of promise.all

--- examples/promises/promise_all.js
+++ examples/promises/promise_all.js
@@ -12,3 +12,19 @@ Promise.all([
 ])
   .then(data => console.log(data))
   .catch(err => console.log(err))
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+function makePromise(value){
+  return new Promise((resolve, reject) => {
+    resolve(value);
+  });
+}
+
+const promise1 = makePromise('first');
+const promise2 = makePromise('second');
+
+Promise.all([promise1, promise2])
+  .then(data => console.log(data))
+
